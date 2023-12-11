159df736545abfb6f62741f423db0f1390bec7a4
CS Zone <shinobikachimiku@gmail.com>
2023-12-11 21:51:55 +0700

    new index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const canva = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.heigth = window.innerHeight
+
+class Player{
+    construct(){
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = 10;
+    }
+}
