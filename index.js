138c980b5223bc8a6b1f99ef1d053c400d6cf450
CS Zone <shinobikachimiku@gmail.com>
2023-12-11 22:25:41 +0700

    new index.js

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const canva = document.querySelector('canvas')
-const c = canvas.getContext('2d')
 
-canvas.width = window.innerWidth
-canvas.heigth = window.innerHeight
 
 class Player{
-    construct(){
-        this.position = position;
-        this.velocity = velocity;
-        this.radius = 10;
+    construct({postition, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15
+    }
+
+    draw(){
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'yellow'
+        c.fill
+        c.closePath
     }
 }
+
+//const map =[]
+//const boundaries = []
+
+const player = new Player({
+    position: {
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.heigth + Boundary.heigth / 2
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+//boundararies.forEach({})
+
+player.draw()
+
+addEventListener('keydown', ({key}) => {
+    console.log(key)
+    switch (key){
+        case 'w':
+        
+        break
+    }
+})
+ 
