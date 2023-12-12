a13fe71a40816c90467efa7a2723d04a3aee49c1
CS Zone <shinobikachimiku@gmail.com>
2023-12-12 09:02:08 +0700

    Add character with movement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const canva = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = window.innerWidth
-canvas.heigth = window.innerHeight
+canvas.height = window.innerHeight
 
 class Player{
     construct({postition, velocity}){
@@ -18,6 +18,13 @@ class Player{
         c.fill
         c.closePath
     }
+
+    update(){
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+    }
 }
 
 //const map =[]
@@ -26,7 +33,7 @@ class Player{
 const player = new Player({
     position: {
         x: Boundary.width + Boundary.width / 2,
-        y: Boundary.heigth + Boundary.heigth / 2
+        y: Boundary.height + Boundary.height / 2
     },
     velocity: {
         x: 0,
@@ -34,16 +41,91 @@ const player = new Player({
     }
 })
 
-//boundararies.forEach({})
+const keys = {
+    w: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
 
-player.draw()
+let lastKey = ''
+
+function animate(){
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    boundaries.forEach((boundary) => {
+        boundary.draw()
+    })
+
+    player.update()
+
+    player.velocity.x = 0
+    player.velocity.y = 0
+    if(key.w.pressed && lastKey === 'w'){
+        player.velocity.y = -5
+    }else if(key.s.pressed && lastKey === 's'){
+        player.velocity.y = 5
+    }
+    else if(key.a.pressed && lastKey === 'a'){
+        player.velocity.x = -5
+    }
+    else if(key.d.pressed && lastKey === 'd'){
+        player.velocity.x = 5
+    }
+}
+
+animate()
 
 addEventListener('keydown', ({key}) => {
     console.log(key)
     switch (key){
         case 'w':
-        
+        keys.w.pressed = true
+        lastKey = 'w'
+        break
+        case 's':
+        keys.s.pressed = true
+        lastKey = 's'
+        break
+        case 'a':
+        keys.a.pressed = true
+        lastKey = 'a'
+        break
+        case 'd':
+        keys.d.pressed = true
+        lastKey = 'd'
         break
     }
+
+})
+
+addEventListener('keyup', ({key}) => {
+    console.log(key)
+    switch (key){
+        case 'w':
+        keys.w.pressed = false
+        break
+        case 's':
+        keys.s.pressed = false
+        break
+        case 'a':
+        keys.a.pressed = false
+        break
+        case 'd':
+        keys.d.pressed = false
+        break
+    }
+
+    console.log(key.d.pressed)
+    console.log(key.s.pressed)
+    
 })
  
