099b54752f80db4fefe502f82c29902ad6bdf78d
phuongnam1556 <trandangphuongnam2003@gmail.com>
2023-12-14 23:00:10 +0700

    remove pellets on collision

--- index.js
+++ index.js
@@ -413,10 +413,25 @@ function animate(){
             }
         }    
     }
-
-    pellets.forEach((pellet) => {
+    
+    for (let i = pellets.length - 1; i > 0; i--){
+        const pellet = pellets[i]
         pellet.draw()
-    })
+
+        if(
+            Math.hypot(
+                pellet.position.x - player.position.x,
+                pellet.position.y - player.position.y
+            ) <
+            pellet.radius + player.radius
+        ){
+            console.log("touching")
+            pellets.splice(i, 1)
+        }
+    }
+
+        
+
 
     boundaries.forEach((boundary) => {
         boundary.draw()
