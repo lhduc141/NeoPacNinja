8f6900d2cb9fccc853c96d6298de954d9a4acf5d
Siuuuu75085 <147909040+Siuuuu75085@users.noreply.github.com>
2024-01-07 20:55:05 +0700

    game.js

--- game.js
+++ game.js
@@ -356,3 +356,40 @@ window.addEventListener("keydown", (event) => {
   }, 1);
 });
 
+let gamePaused = false;
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    gamePause();
+  } 
+});
+
+
+let gameContinue = () => {
+  if (gamePaused) {
+    gamePaused = false;
+    gameInterval = setInterval(gameLoop,10);
+  }
+};
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    gameContinue();
+  }
+});
+
+
+let gamePause = () => {
+  if (!gamePaused) {
+    gamePaused = true;
+    drawGamePaused();
+    clearInterval(gameInterval);
+  }
+};
+
+let drawGamePaused = () => {
+  canvasContext.font = "40px Emulogic";
+  canvasContext.fillStyle = "white";
+  canvasContext.fillText("Game Paused!", 110, 240);
+};
+
